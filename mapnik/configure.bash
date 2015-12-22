69411384638462629b60139abc3f07c11b11022a
Joshua Chamberlain <josh@zephyri.co>
2015-12-22 14:09:26 -0800

    Use forked version of c api

--- mapnik/configure.bash
+++ mapnik/configure.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-[ -f mapnik_c_api.cpp ] || curl -LO https://raw.github.com/fawick/mapnik-c-api/master/mapnik_c_api.cpp
-[ -f mapnik_c_api.h ] || curl -LO https://raw.github.com/fawick/mapnik-c-api/master/mapnik_c_api.h
+[ -f mapnik_c_api.cpp ] || curl -LO https://raw.githubusercontent.com/geostrategies/mapnik-c-api/copy-map/mapnik_c_api.cpp
+[ -f mapnik_c_api.h ] || curl -LO https://raw.githubusercontent.com/geostrategies/mapnik-c-api/copy-map/mapnik_c_api.h
 
 cat > gen_import.go <<EOF
 package mapnik
